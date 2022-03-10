29becaade7ec5189ff5e7946f8e79033668793af
Felipe Leão <felipe@felipeleao.com>
2022-03-09 22:06:33 -0300

    Create Campaign Factory

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,5 +1,19 @@
 pragma solidity ^0.4.17;
 
+contract CampaignFactory {
+  address[] public deployedCampaigns;
+
+  function createCampaign(uint minimum) public {
+    address newCampaign = new Campaign(minimum, msg.sender);
+
+    deployedCampaigns.push(newCampaign);
+  }
+
+  function getDeployedCampaigns() public view returns (address[]) {
+    return deployedCampaigns;
+  }
+}
+
 contract Campaign {
   struct Request {
     string description;
@@ -21,8 +35,8 @@ contract Campaign {
     _;
   }
 
-  function Campaign(uint minimum) public {
-    manager = msg.sender;
+  function Campaign(uint minimum, address creator) public {
+    manager = creator;
     minimumContribution = minimum;
   }
 
